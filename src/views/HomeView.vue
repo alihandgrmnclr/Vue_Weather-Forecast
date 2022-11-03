e44bae18af50cf50e976347897b1f65dc0e891cf
Alihan <alihandegirmenciler@gmail.com>
2022-11-03 17:37:12 +0300

    home added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+import Forecast from "../components/Forecast.vue"
+
+const city = ref("");
+const weather = ref(false);
+const weatherData = ref(null);
+
+
+</script>
+
+<template>
+
+
+  <div class="weather-forecast">
+    <template v-if="!weather">
+        <Forecast :data="weatherData"></Forecast>
+    </template>
+
+  </div>
+
+</template>
+
+<style scoped>
+.weather-forecast {
+  @apply h-[95vh];
+}
+</style>
